40d05ba189b28b49eae97bf76433b85e89680b00
VladimirKvit <cheremuha1@gmail.com>
2020-11-20 00:53:43 +0300

    mogeh tut gnat kak hocheh

---
+++ Dog.java
@@ -0,0 +1,9 @@
+public class Dog {
+double rost;
+double ves;
+String cvet;
+public void Lara(){
+    System.out.println("lara"+rost+ves+cvet);
+}
+
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+Dog dog=new Dog();
+dog.rost=22.5;
+dog.ves=3.55;
+dog.cvet="black";
+dog.Lara();
+    }
+}
